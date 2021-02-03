fb5da22ed7d08d6bfb4c2efa1937d6a1c4ac8c17
Samuel Andrews <sam8148@outlook.com>
2021-02-03 16:27:58 -0600

    Implemented the three defined features and test function for all three

--- composable_pandas/str.py
+++ composable_pandas/str.py
@@ -43,3 +43,204 @@ def capitalize(col):
     
     """
     return col.str.capitalize()
+
+
+
+
+@pipeable
+def isalnum(col):
+    """isalnum() method of pandas.core.strings.accessor.StringMethods instance
+    Check whether all characters in each string are alphanumeric.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.isalnum` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    isalpha : Check whether all characters are alphabetic.
+    isdigit : Check whether all characters are digits.
+    isdecimal : Check whether all characters are decimal.
+    isnumeric : Check whether all characters are numeric.
+    
+    Examples
+    --------
+    **Checks for Alphabetic and Numeric Characters**
+    
+    >>> s1 = pd.Series(['one', 'one1', '1', ''])
+    
+    >>> s1 >> isalpha()
+    0     True
+    1    False
+    2    False
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isnumeric()
+    0    False
+    1    False
+    2     True
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isalnum()
+    0     True
+    1     True
+    2     True
+    3    False
+    dtype: bool
+    
+    Note that checks against characters mixed with any additional punctuation
+    or whitespace will evaluate to false for an alphanumeric check.
+    
+    >>> s2 = pd.Series(['A B', '1.5', '3,000'])
+    >>> s2 >> isalnum()
+    0    False
+    1    False
+    2    False
+    dtype: bool
+    
+    
+    
+    """
+    return col.str.isalnum()
+
+
+
+
+@pipeable
+def isalpha(col):
+    """isalpha() method of pandas.core.strings.accessor.StringMethods instance
+    Check whether all characters in each string are alphabetic.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.isalpha` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    isalnum : Check whether all characters are alphanumeric.
+    isdigit : Check whether all characters are digits.
+    isdecimal : Check whether all characters are decimal.
+    isnumeric : Check whether all characters are numeric.
+    
+    Examples
+    --------
+    **Checks for Alphabetic and Numeric Characters**
+    
+    >>> s1 = pd.Series(['one', 'one1', '1', ''])
+    
+    >>> s1 >> isalpha()
+    0     True
+    1    False
+    2    False
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isnumeric()
+    0    False
+    1    False
+    2     True
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isalnum()
+    0     True
+    1     True
+    2     True
+    3    False
+    dtype: bool
+    
+    Note that checks against characters mixed with any additional punctuation
+    or whitespace will evaluate to false for an alphanumeric check.
+    
+    >>> s2 = pd.Series(['A B', '1.5', '3,000'])
+    >>> s2 >> isalnum()
+    0    False
+    1    False
+    2    False
+    dtype: bool
+    
+    
+    
+    """
+    return col.str.isalpha()   
+
+
+
+
+@pipeable
+def isdigit(col):
+    """isdigit() method of pandas.core.strings.accessor.StringMethods instance
+    Check whether all characters in each string are digits.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.isdigit` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    isalpha : Check whether all characters are alphabetic.
+    isalnum : Check whether all characters are alphanumeric.
+    isdecimal : Check whether all characters are decimal.
+    isnumeric : Check whether all characters are numeric.
+    
+    Examples
+    --------
+    **Checks for Alphabetic and Numeric Characters**
+    
+    >>> s1 = pd.Series(['one', 'one1', '1', ''])
+    
+    >>> s1 >> isalpha()
+    0     True
+    1    False
+    2    False
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isnumeric()
+    0    False
+    1    False
+    2     True
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isalnum()
+    0     True
+    1     True
+    2     True
+    3    False
+    dtype: bool
+    
+    Note that checks against characters mixed with any additional punctuation
+    or whitespace will evaluate to false for an alphanumeric check.
+    
+    >>> s2 = pd.Series(['A B', '1.5', '3,000'])
+    >>> s2 >> isalnum()
+    0    False
+    1    False
+    2    False
+    dtype: bool
+    
+    
+    
+    """
+    return col.str.isdigit()       
--- tests/test_str.py
+++ tests/test_str.py
@@ -1,10 +1,11 @@
-from composable_pandas.str import capitalize
-from datetime import datetime
+from composable_pandas.str import capitalize, isalnum, isalpha, isdigit
+from datetime import datetime, timedelta
 
 import numpy as np
 import pytest
 
 from pandas import Series, _testing as tm
+from pandas import DataFrame, Index, MultiIndex, Series, isna, notna
 
 def test_capitalize():
     values = Series(["FOO", "BAR", np.nan, "Blah", "blurg"])
@@ -17,3 +18,22 @@ def test_capitalize():
     mixed = mixed >> capitalize()
     exp = Series(["Foo", np.nan, "Bar", np.nan, np.nan, "Blah", np.nan, np.nan, np.nan])
     tm.assert_almost_equal(mixed, exp)
+
+
+
+
+def test_ismethods():
+    values = ["A", "b", "Xy", "4", "3A", "", "TT", "55", "-", "  "]
+    str_s = Series(values)
+    alnum_e = [True, True, True, True, True, False, True, True, False, False]
+    alpha_e = [True, True, True, False, False, False, True, False, False, False]
+    digit_e = [False, False, False, True, False, False, False, True, False, False]
+
+    tm.assert_series_equal(str_s >> isalnum(), Series(alnum_e))
+    tm.assert_series_equal(str_s >> isalpha(), Series(alpha_e))
+    tm.assert_series_equal(str_s >> isdigit(), Series(digit_e))
+
+
+    assert (str_s >> isalnum()).tolist() == [v.isalnum() for v in values]
+    assert (str_s >> isalpha()).tolist() == [v.isalpha() for v in values]
+    assert (str_s >> isdigit()).tolist() == [v.isdigit() for v in values]
