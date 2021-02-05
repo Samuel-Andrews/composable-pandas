40834de84907bedf43b090146a1fc10199b2659c
Samuel Andrews <sam8148@outlook.com>
2021-02-05 13:43:22 -0600

    Creating branch with all features for turn in

--- composable_pandas/str.py
+++ composable_pandas/str.py
@@ -2,6 +2,8 @@ from composable import pipeable
 import pandas as pd
 
 
+
+
 @pipeable
 def capitalize(col):
     """Convert strings in the Series/Index to be capitalized.
@@ -43,6 +45,36 @@ def capitalize(col):
     
     """
     return col.str.capitalize()
+
+
+
+
+@pipeable
+def index(sub, col, *, start = 0, end = None):
+    """index(sub, start=0, end=None) method of pandas.core.strings.accessor.StringMethods instance
+    Return lowest indexes in each string in Series/Index.
+    
+    Each of the returned indexes corresponds to the position where the
+    substring is fully contained between [start:end]. This is the same
+    as ``str.find`` except instead of returning -1, it raises a
+    ValueError when the substring is not found. Equivalent to standard
+    ``str.index``.
+    
+    Parameters
+    ----------
+    sub : str
+        Substring being searched.
+    start : int
+        Left edge index.
+    end : int
+        Right edge index.
+    
+    Returns
+    -------
+    Series or Index of object
+
+    """
+    return col.str.index(sub = sub, start = start, end = end)
 
 
 
@@ -182,6 +214,73 @@ def isalpha(col):
 
 
 @pipeable
+def isdecimal(col):
+    """isdecimal() method of pandas.core.strings.accessor.StringMethods instance
+    Check whether all characters in each string are decimal.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.isdecimal` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    isalpha : Check whether all characters are alphabetic.
+    isalnum : Check whether all characters are alphanumeric.
+    isdigit : Check whether all characters are digits.
+    isnumeric : Check whether all characters are numeric.
+    
+    Examples
+    --------
+    **Checks for Alphabetic and Numeric Characters**
+    
+    >>> s1 = pd.Series(['one', 'one1', '1', ''])
+    
+    >>> s1 >> isalpha()
+    0     True
+    1    False
+    2    False
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isnumeric()
+    0    False
+    1    False
+    2     True
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isalnum()
+    0     True
+    1     True
+    2     True
+    3    False
+    dtype: bool
+    
+    Note that checks against characters mixed with any additional punctuation
+    or whitespace will evaluate to false for an alphanumeric check.
+    
+    >>> s2 = pd.Series(['A B', '1.5', '3,000'])
+    >>> s2 >> isalnum()
+    0    False
+    1    False
+    2    False
+    dtype: bool
+    
+    
+    
+    """
+    return col.str.isdecimal()       
+
+
+
+
+@pipeable
 def isdigit(col):
     """isdigit() method of pandas.core.strings.accessor.StringMethods instance
     Check whether all characters in each string are digits.
@@ -244,3 +343,71 @@ def isdigit(col):
     
     """
     return col.str.isdigit()       
+
+
+
+
+@pipeable
+def isnumeric(col):
+    """isnumeric() method of pandas.core.strings.accessor.StringMethods instance
+    Check whether all characters in each string are numeric.
+    
+    This is equivalent to running the Python string method
+    :meth:`str.isnumeric` for each element of the Series/Index. If a string
+    has zero characters, ``False`` is returned for that check.
+    
+    Returns
+    -------
+    Series or Index of bool
+        Series or Index of boolean values with the same length as the original
+        Series/Index.
+    
+    See Also
+    --------
+    isalpha : Check whether all characters are alphabetic.
+    isalnum : Check whether all characters are alphanumeric.
+    isdecimal : Check whether all characters are decimal.
+    isdigit : Check whether all characters are digits.
+
+    
+    Examples
+    --------
+    **Checks for Alphabetic and Numeric Characters**
+    
+    >>> s1 = pd.Series(['one', 'one1', '1', ''])
+    
+    >>> s1 >> isalpha()
+    0     True
+    1    False
+    2    False
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isnumeric()
+    0    False
+    1    False
+    2     True
+    3    False
+    dtype: bool
+    
+    >>> s1 >> isalnum()
+    0     True
+    1     True
+    2     True
+    3    False
+    dtype: bool
+    
+    Note that checks against characters mixed with any additional punctuation
+    or whitespace will evaluate to false for an alphanumeric check.
+    
+    >>> s2 = pd.Series(['A B', '1.5', '3,000'])
+    >>> s2 >> isalnum()
+    0    False
+    1    False
+    2    False
+    dtype: bool
+    
+    
+    
+    """
+    return col.str.isnumeric()      
--- tests/test_str.py
+++ tests/test_str.py
@@ -1,4 +1,4 @@
-from composable_pandas.str import capitalize, isalnum, isalpha, isdigit
+from composable_pandas.str import capitalize, index, isalnum, isalpha, isdecimal, isdigit, isnumeric
 from datetime import datetime, timedelta
 
 import numpy as np
@@ -6,6 +6,9 @@ import pytest
 
 from pandas import Series, _testing as tm
 from pandas import DataFrame, Index, MultiIndex, Series, isna, notna
+
+
+
 
 def test_capitalize():
     values = Series(["FOO", "BAR", np.nan, "Blah", "blurg"])
@@ -19,6 +22,47 @@ def test_capitalize():
     exp = Series(["Foo", np.nan, "Bar", np.nan, np.nan, "Blah", np.nan, np.nan, np.nan])
     tm.assert_almost_equal(mixed, exp)
 
+
+
+
+def test_index():
+    def _check(result, expected):
+        if isinstance(result, Series):
+            tm.assert_series_equal(result, expected)
+        else:
+            tm.assert_index_equal(result, expected)
+
+    for klass in [Series, Index]:
+        s = klass(["ABCDEFG", "BCDEFEF", "DEFGHIJEF", "EFGHEF"])
+
+        result = s >> index("EF")
+        _check(result, klass([4, 3, 1, 0]))
+        expected = np.array([v.index("EF") for v in s.values], dtype=np.int64)
+        tm.assert_numpy_array_equal(result.values, expected)
+
+        result = s >> index("EF", start = 3)
+        _check(result, klass([4, 3, 7, 4]))
+        expected = np.array([v.index("EF", 3) for v in s.values], dtype=np.int64)
+        tm.assert_numpy_array_equal(result.values, expected)
+
+
+        result = s >> index("E", start = 4, end = 8)
+        _check(result, klass([4, 5, 7, 4]))
+        expected = np.array([v.index("E", 4, 8) for v in s.values], dtype=np.int64)
+        tm.assert_numpy_array_equal(result.values, expected)
+
+        with pytest.raises(ValueError, match="substring not found"):
+            result = s >> index("DE")
+
+        msg = "expected a string object, not int"
+        with pytest.raises(TypeError, match=msg):
+            result = s >> index(0)
+
+    # test with nan
+    s = Series(["abcb", "ab", "bcbe", np.nan])
+    result = s >> index("b")
+    tm.assert_series_equal(result, Series([1, 1, 0, np.nan]))
+  
 
 
 
@@ -37,3 +81,29 @@ def test_ismethods():
     assert (str_s >> isalnum()).tolist() == [v.isalnum() for v in values]
     assert (str_s >> isalpha()).tolist() == [v.isalpha() for v in values]
     assert (str_s >> isdigit()).tolist() == [v.isdigit() for v in values]
+
+
+
+
+def test_isnumeric():
+    # 0x00bc: ¼ VULGAR FRACTION ONE QUARTER
+    # 0x2605: ★ not number
+    # 0x1378: ፸ ETHIOPIC NUMBER SEVENTY
+    # 0xFF13: ３ Em 3
+    values = ["A", "3", "¼", "★", "፸", "３", "four"]
+    s = Series(values)
+    numeric_e = [False, True, True, False, True, True, False]
+    decimal_e = [False, True, False, False, False, True, False]
+    tm.assert_series_equal(s >> isnumeric(), Series(numeric_e))
+    tm.assert_series_equal(s >> isdecimal(), Series(decimal_e))
+
+    unicodes = ["A", "3", "¼", "★", "፸", "３", "four"]
+    assert (s >> isnumeric()).tolist() == [v.isnumeric() for v in unicodes]
+    assert (s >> isdecimal()).tolist() == [v.isdecimal() for v in unicodes]
+
+    values = ["A", np.nan, "¼", "★", np.nan, "３", "four"]
+    s = Series(values)
+    numeric_e = [False, np.nan, True, False, np.nan, True, False]
+    decimal_e = [False, np.nan, False, False, np.nan, True, False]
+    tm.assert_series_equal(s >> isnumeric(), Series(numeric_e))
+    tm.assert_series_equal(s >> isdecimal(), Series(decimal_e))
