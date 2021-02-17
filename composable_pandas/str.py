753a3a53f46201300330914f067f6d14873c2b65
Samuel Andrews <sam8148@outlook.com>
2021-02-16 18:36:37 -0600

    All_working

--- composable_pandas/str.py
+++ composable_pandas/str.py
@@ -43,3 +43,145 @@ def capitalize(col):
     
     """
     return col.str.capitalize() ###
+
+@pipeable
+def minute(col):
+    """The minutes of the datetime.
+    
+    Examples
+    --------
+    >>> datetime_series = pd.Series(
+    ...     pd.date_range("2000-01-01", periods=3, freq="T")
+    ... )
+    >>> datetime_series
+    0   2000-01-01 00:00:00
+    1   2000-01-01 00:01:00
+    2   2000-01-01 00:02:00
+    dtype: datetime64[ns]
+    >>> datetime_series >> minute()
+    0    0
+    1    1
+    2    2
+    dtype: int64
+
+    """
+    return col.dt.minute
+
+@pipeable
+def month(col):
+    """The month as January=1, December=12.
+    
+    Example
+    --------
+    >>> datetime_series = pd.Series(
+    ...     pd.date_range("2000-01-01", periods=3, freq="M")
+    ... )
+    >>> datetime_series
+    0   2000-01-31
+    1   2000-02-29
+    2   2000-03-31
+    dtype: datetime64[ns]
+    >>> datetime_series >> month()
+    0    1
+    1    2
+    2    3
+    dtype: int64
+
+    """
+    return col.dt.month
+
+
+@pipeable
+def quarter(col):
+    """The quarter of the date.
+        1 = Jan, Feb, Mar
+        2 = Apr, May, June
+        3 = July, Aug, Sept
+        4 = Oct, Nov, Dec
+    Example
+    --------
+    >>> datetime_series = pd.Series(
+    ...     pd.date_range("2000-01-01", periods=3, freq="Q")
+    ... )
+    >>> datetime_series
+    0   2000-01-31
+    1   2000-06-30
+    2   2000-09-30
+    dtype: datetime64[ns]
+    >>> datetime_series >> quarter()
+    0    1
+    1    2
+    2    3
+    dtype: int64
+
+    """
+    return col.dt.quarter
+
+@pipeable
+def week(col):
+    """The week ordinal of the year.
+
+    Example
+    --------
+    >>> datetime_series = pd.Series(
+    ...     pd.date_range("2000-01-01", periods=5, freq="W")
+    ... )
+    >>> datetime_series
+    0   2000-01-02
+    1   2000-01-09
+    2   2000-01-16
+    3   2000-01-23
+    4   2000-01-30
+    dtype: datetime64[ns]
+    >>> datetime_series >> week()
+    0    52
+    1    1
+    2    2
+    3    3
+    4    4
+    dtype: int64
+
+    """
+
+    return col.dt.week
+
+
+@pipeable
+def is_leap_year(col):
+    """Boolean indicator if the date belongs to a leap year.
+    
+    A leap year is a year, which has 366 days (instead of 365) including
+    29th of February as an intercalary day.
+    Leap years are years which are multiples of four with the exception
+    of years divisible by 100 but not by 400.
+    
+    Returns
+    -------
+    Series or ndarray
+         Booleans indicating if dates belong to a leap year.
+    
+    Examples
+    --------
+    This method is available on Series with datetime values under
+    the ``.dt`` accessor, and directly on DatetimeIndex.
+    
+    >>> idx = pd.date_range("2012-01-01", "2015-01-01", freq="Y")
+    >>> idx
+    DatetimeIndex(['2012-12-31', '2013-12-31', '2014-12-31'],
+                  dtype='datetime64[ns]', freq='A-DEC')
+    >>> idx.is_leap_year
+    array([ True, False, False])
+    
+    >>> dates_series = pd.Series(idx)
+    >>> dates_series
+    0   2012-12-31
+    1   2013-12-31
+    2   2014-12-31
+    dtype: datetime64[ns]
+    >>> dates_series.dt.is_leap_year
+    0     True
+    1    False
+    2    False
+    dtype: bool
+    """
+    return col.dt.is_leap_year
--- tests/test_str.py
+++ tests/test_str.py
@@ -1,8 +1,9 @@
-from composable_pandas.str import capitalize
+from composable_pandas.str import capitalize, is_leap_year, minute, month, quarter, week
 from datetime import datetime
 
 import numpy as np
 import pytest
+import pandas as pd
 
 from pandas import Series, _testing as tm
 
@@ -17,3 +18,62 @@ def test_capitalize():
     mixed = mixed >> capitalize()
     exp = Series(["Foo", np.nan, "Bar", np.nan, np.nan, "Blah", np.nan, np.nan, np.nan])
     tm.assert_almost_equal(mixed, exp)
+
+
+def test_minute():
+    result = pd.Series(pd.date_range("2000-01-01", periods=12, freq="T"))
+    exp = Series(range(0,12))
+    tm.assert_series_equal(result >> minute(), exp)
+
+    # roll over
+    roll = pd.Series(pd.date_range("2000-01-01 00:59:00", periods=3, freq="T"))
+    exp = Series([59,0,1])
+    tm.assert_series_equal(roll.tail(3) >> minute(), exp)
+
+
+def test_month():
+    result = pd.Series(pd.date_range("2000-01-01", periods=12, freq="M"))
+    exp = Series(range(1,13))
+    tm.assert_series_equal(result >> month(), exp)
+
+    # roll over
+    roll = pd.Series(pd.date_range("2000-12-01", periods=3, freq="M"))
+    exp = Series([12,1,2])
+    tm.assert_series_equal(roll.tail(3) >> month(), exp)
+
+
+def test_quarter():
+    result = pd.Series(pd.date_range("2000-01-01", periods=4, freq="Q"))
+    exp = Series(range(1,5))
+    tm.assert_series_equal(result >> quarter(), exp)
+
+    # roll over
+    roll = pd.Series(pd.date_range("2000-12-01", periods=5, freq="Q"))
+    exp = Series([4,1,2,3,4])
+    tm.assert_series_equal(roll >> quarter(), exp)
+
+
+
+def test_week():
+    result = pd.Series(pd.date_range("2000-01-05", periods=12, freq="W"))
+    exp = Series(range(1,13))
+    tm.assert_series_equal(result >> week(), exp)
+
+    # roll over
+    roll = pd.Series(pd.date_range("2000-12-31", periods=3, freq="W"))
+    exp = Series([52,1,2])
+    tm.assert_series_equal(roll >> week(), exp)
+
+
+def test_is_leap_year():
+        dt = pd.Series(pd.date_range("2000-01-01", periods=1, freq="Y"))
+        assert (dt >> is_leap_year())[0]
+
+        dt = pd.Series(pd.date_range("1999-01-01", periods=1, freq="Y"))
+        assert not (dt >> is_leap_year())[0]
+
+        dt = pd.Series(pd.date_range("2004-01-01", periods=1, freq="Y"))
+        assert (dt >> is_leap_year())[0]
+
+        dt = pd.Series(pd.date_range("2100-01-01", periods=1, freq="Y"))
+        assert not (dt >> is_leap_year())[0]
